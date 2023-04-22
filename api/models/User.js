2b8214f80ccdd39807a4bf32420f9c969552fbff
cNoveron <carlos.escom.ipn@gmail.com>
2023-04-22 15:30:44 -0500

    avoid db connection

--- api/models/User.js
+++ api/models/User.js
@@ -11,23 +11,23 @@ const hooks = {
 
 const tableName = 'users';
 
-const User = sequelize.define('User', {
-  email: {
-    type: Sequelize.STRING,
-    unique: true,
-  },
-  password: {
-    type: Sequelize.STRING,
-  },
-}, { hooks, tableName });
+// const User = sequelize.define('User', {
+//   email: {
+//     type: Sequelize.STRING,
+//     unique: true,
+//   },
+//   password: {
+//     type: Sequelize.STRING,
+//   },
+// }, { hooks, tableName });
 
-// eslint-disable-next-line
-User.prototype.toJSON = function () {
-  const values = Object.assign({}, this.get());
+// // eslint-disable-next-line
+// User.prototype.toJSON = function () {
+//   const values = Object.assign({}, this.get());
 
-  delete values.password;
+//   delete values.password;
 
-  return values;
-};
+//   return values;
+// };
 
-module.exports = User;
+// module.exports = User;
