eda0994de59a7a493d9dac65311e06a54b7c97b1
cNoveron <carlos.escom.ipn@gmail.com>
2023-04-23 15:55:37 -0500

    improve array-building efficiency

--- api/functions/possibleFixablesDeep.js
+++ api/functions/possibleFixablesDeep.js
@@ -1,14 +1,15 @@
+const _ = require('lodash');
 const possibleFixables = require('./possibleFixables');
 
-module.exports = function(seq, depth = 2) {
-    const depth1_possibleFixables = possibleFixables(seq)
-    console.log(depth1_possibleFixables);
+module.exports = function(seq, depth = 1) {
+    let depth1_possibleFixables = possibleFixables(seq)
 
     for (let i = 1; i < depth; i++) {
         let r = depth1_possibleFixables.flatMap(s => possibleFixables(s))
-        depth1_possibleFixables.push(r)
+        // console.log(r);
+        depth1_possibleFixables = _.union(depth1_possibleFixables, r)
+        // console.log(depth1_possibleFixables);
     }
-    console.log(depth1_possibleFixables);
 
     return depth1_possibleFixables;
 }
--- api/functions/possibleFixesDeep.js
+++ api/functions/possibleFixesDeep.js
@@ -1,11 +1,16 @@
+const _ = require('lodash');
 const possibleFixes = require('./possibleFixes');
 
-module.exports = function(seq, depth = 2) {
+module.exports = function(seq, depth = 1) {
     const depth1_possibleFixes = possibleFixes(seq)
+    // console.log(depth1_possibleFixes);
+    // console.log('-------');
 
     for (let i = 1; i < depth; i++) {
         let r = depth1_possibleFixes.flatMap(s => possibleFixes(s))
-        depth1_possibleFixes.push(r)
+        // console.log(r);
+        depth1_possibleFixes = _.union(depth1_possibleFixes, r)
+        // console.log(depth1_possibleFixes);
     }
 
     return depth1_possibleFixes;
