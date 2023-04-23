9c0041afc88a2c12aa67c7575de33859ec822746
cNoveron <carlos.escom.ipn@gmail.com>
2023-04-23 15:54:10 -0500

    improve possible fix finding

--- api/functions/possibleFixables.js
+++ api/functions/possibleFixables.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
-
 require.extensions['.txt'] = function (module, filename) {
     module.exports = fs.readFileSync(filename, 'utf8');
 };
-
 const dictionary = require("../resources/dictionary.txt");
+
+
 
 //This function first defines an array of vowels to use, and then loops through the input sequence and adds each vowel between consecutive pairs of letters. It then checks whether each resulting word can form a valid English word using the checkWord() function from my previous answer. The function returns true if at least one valid English word can be formed, and false otherwise.
 module.exports = function(seq) {
     // Define an array of vowels to use
     const vowels = ["a", "e", "i", "o", "u"];
 
-    const possibleFixables = []
+    let possibleFixables = []
 
     // Loop through the sequence and add each vowel between consecutive pairs of letters
     for (let i = -1; i < seq.length; i++) {
         for (const vowel of vowels) {
             const newWord = i == -1 
                 ? vowel + seq
-                : seq.slice(0, i + 1) + vowel + seq.slice(i + 1);
-            // console.log(newWord);
-            regexString = `(\\B` + `${newWord}` + `\\B)`;
+                : seq.slice(0, i+1) + vowel + seq.slice(i+1);
+            const fragment = i < 1
+                ? newWord.slice(0,5)
+                : newWord.slice(i-1,i+4);
+            
+            if (fragment.length < 4)
+                continue;
+
+            regexString = seq.length < 5
+                ? `\\b${fragment}|\\b\\B${fragment}|${fragment}\\B\\b|${fragment}\\b`
+                : i < 2
+                    ? `${fragment}\\B|${fragment}\\b`
+                    : seq.length-4 < i
+                        ? `\\B${fragment}|\\b${fragment}`
+                        : `\\B${fragment}\\B`
+
             regex = new RegExp(regexString, "dg");
             matches = dictionary.match(regex);
 
@@ -29,9 +42,6 @@ module.exports = function(seq) {
             }
         }
     }
-    // console.log('-----');
-    // console.log(possibleFixables);
-    // console.log('-----');
 
     return possibleFixables;
 }
