a62e487e1c3083e6b32fe9a9b31dc17914768ea4
cNoveron <carlos.escom.ipn@gmail.com>
2023-04-24 14:11:39 -0500

    introduce app component base code

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,117 +1,114 @@
-import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Search from "react-searchbox-awesome";
+import "./styles.css";
+/*
+example data. USA states.
+the componenet requires an array of object with a property "title".
+  []{title: string}
+*/
+import { states } from "./states.data";
 
-import { actions } from '../redux/modules/account';
+function App() {
+  const [filtered, setFiltered] = useState([]);
 
-const propTypes = {};
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      burgerOpen: false
-    };
-    this.addBurgerOpenClass = this.addBurgerOpenClass.bind(this);
-  }
-
-  addBurgerOpenClass(classes) {
-    const { burgerOpen } = this.state;
-    if (burgerOpen) {
-      classes = [...classes, 'is-active'];
+  // here the data is filtered as you search
+  const inputHandler = e => {
+    const input = e.target.value.toLowerCase();
+    if (input.length < 3) {
+      setFiltered([]);
+    } else {
+      const result = states.filter(obj => {
+        return obj.name.toLowerCase().includes(input);
+      });
+      setFiltered(result);
     }
-    return classes.join(' ');
-  }
+  };
 
-  render() {
-    const { props, state: { burgerOpen } } = this;
-    return (
-      <div>
-        <nav className="navbar is-transparent">
-          <div className="navbar-brand">
-            <Link className="navbar-item" to="/">
-              <img
-                src="/clevertech.svg"
-                alt="Clevertech React Boilerplate"
-                width="112"
-                height="28"
-              />
-            </Link>
-            <div
-              className={this.addBurgerOpenClass(['navbar-burger', 'burger'])}
-              data-target="navbarExampleTransparentExample"
-              onClick={() => this.setState({ burgerOpen: !burgerOpen })}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
+  /*
+    here you define what happens when you press enter. 
+    note that the data that is passed to the list element, is stored in the data-set attribute.
+  */
+  const enterHandler = e => {
+    const searchitem = JSON.parse(e.target.dataset.searchitem);
+    console.log("Enter pressed", searchitem);
+  };
 
-          <div
-            id="navbarExampleTransparentExample"
-            className={this.addBurgerOpenClass(['navbar-menu'])}
-          >
-            <div className="navbar-start">
-              <Link className="navbar-item" to="/">
-                Home
-              </Link>
-              {props.user && (
-                <NavLink className="navbar-item" to="/products">
-                  Products
-                </NavLink>
-              )}
-            </div>
+  // same as above
+  const clickHandler = e => {
+    const searchitem = JSON.parse(e.target.dataset.searchitem);
+    console.log("Click click!", searchitem);
+  };
 
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="field is-grouped">
-                  {props.user && (
-                    <p className="control">
-                      <NavLink className="button is-light" to="/account/settings">
-                        <span className="icon">
-                          <i className="fas fa-cogs" />
-                        </span>
-                        <span>Settings</span>
-                      </NavLink>
-                    </p>
-                  )}
-                  {!props.user && (
-                    <p className="control">
-                      <NavLink className="button is-primary" to="/account/login">
-                        <span className="icon">
-                          <i className="fas fa-sign-in-alt" />
-                        </span>
-                        <span>Login</span>
-                      </NavLink>
-                    </p>
-                  )}
-                  {props.user && (
-                    <p className="control">
-                      <a href="#!" onClick={props.logout} className="navbar-item">
-                        Log Out
-                      </a>
-                    </p>
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
-        <div className="content">{props.children}</div>
-      </div>
-    );
-  }
+  // what to happen when escape is pressed. in our example - nothing.
+  const escHandler = e => {
+    console.log("Escape pressed");
+  };
+
+  // this is to close the searchlist when you click outside of it.
+  const clickOutsideHandler = e => {
+    console.log(e.target);
+    if (!e.target.closest(".ReactSearchboxAwesome")) {
+      setFiltered([]);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", clickOutsideHandler);
+    return () => document.removeEventListener("click", clickOutsideHandler);
+  }, []);
+
+  /* the style defined here is passed to child elements
+  note: children inherit some styles like font size, color, line-height...
+  there are some default styles as well.
+  */
+  const style = {
+    width: "calc(80% + (100vw - 100%))",
+    color: "#333", // children inherit
+    backgroundColor: "white", // children inherit
+    fontSize: "2.5rem", // children inherit
+    position: "absolute",
+    top: "3rem",
+    boxShadow: "0 0 28px 2px rgba(0,0,0,0.1)",
+    border: "none",
+    overflow: "hidden"
+  };
+
+  const style1 = {
+    ...style,
+    borderRadius: "15px",
+    backgroundColor: "rgba(250,250,250,0.2)"
+  };
+
+  // thats the style for the active element (hover, focus)
+  const activeStyle = {
+    backgroundColor: "pink"
+  };
+
+  const activeStyle1 = {
+    backgroundImage:
+      "linear-gradient(319deg, #bbff99 0%, #ffec99 37%, #ff9999 100%)"
+  };
+
+  const activeStyle2 = {
+    backgroundColor: "rgba(255,230,230,.3)"
+  };
+
+  return (
+    <div className={"App"}>
+      <Search
+        data={filtered} // array of the objects is passed here. []{title: string}. each object is saved in dataset of the correspondent element.
+        mapping={{ title: "name" }} // when they don't correspond, allows to map the title of the search item and the name property in the filtered data.
+        style={style1} // child elements inherit some styles.
+        activeStyle={activeStyle2} // hover, focus, active color.
+        placeholder={"Search for states..."} // input placeholder.
+        shortcuts={true} // hide or show span elements that display shortcuts.
+        onEnter={enterHandler} // applies only to the list "li" element
+        onInput={inputHandler}
+        onClick={clickHandler} // applies only to the list "li" element
+        onEsc={escHandler} // applies to the entire component
+      />
+    </div>
+  );
 }
 
-const mapStateToProps = (state, { location }) => ({
-  location,
-  user: state.account.user
-});
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  logout: () => dispatch(actions.logout())
-});
-
-App.propTypes = propTypes;
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default App;
